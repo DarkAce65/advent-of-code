43224b1dbdae9fb795e62ad29b6cead38353af34
DarkAce65 <tvdood99@gmail.com>
2021-12-21 01:58:21 -0500

    Solve part 2

--- 2021/day21.py
+++ 2021/day21.py
@@ -1,4 +1,11 @@
 from pathlib import Path
+from typing import Counter
+
+POSSIBLE_ROLLS: list[int] = []
+for r1 in range(1, 4):
+    for r2 in range(1, 4):
+        for r3 in range(1, 4):
+            POSSIBLE_ROLLS.append(r1 + r2 + r3)
 
 
 def parse_starting_positions(problem_input: list[str]) -> list[int]:
@@ -31,8 +38,46 @@ def part_one(starting_positions: list[int]) -> int:
     return min(scores) * num_rolls
 
 
+def find_winner_counts(
+    positions: list[int], scores: list[int], turn: int = 0
+) -> Counter[int]:
+    c: Counter[int] = Counter()
+    next_turn = (turn + 1) % len(positions)
+
+    for possible_roll in POSSIBLE_ROLLS:
+        p = positions.copy()
+        s = scores.copy()
+        p[turn] = (p[turn] - 1 + possible_roll) % 10 + 1
+        s[turn] += p[turn]
+
+        if s[turn] >= 21:
+            c += Counter([turn])
+        else:
+            c += find_winner_counts_cached(p, s, next_turn)
+
+    return c
+
+
+cache: dict[str, Counter[int]] = {}
+cache_stats: list[int] = [0, 0]
+
+
+def find_winner_counts_cached(
+    positions: list[int], scores: list[int], turn: int = 0
+) -> Counter[int]:
+    cache_key = "".join(map(str, positions)) + "".join(map(str, scores)) + str(turn)
+    if cache_key not in cache:
+        cache_stats[1] += 1
+        cache[cache_key] = find_winner_counts(positions, scores, turn)
+    else:
+        cache_stats[0] += 1
+
+    return cache[cache_key]
+
+
 def part_two(starting_positions: list[int]) -> int:
-    pass
+    winner_counts = find_winner_counts_cached(starting_positions, [0, 0])
+    return max(winner_counts.values())
 
 
 if __name__ == "__main__":
