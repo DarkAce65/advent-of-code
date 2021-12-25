d833700b52eca6d65921872a5fefbeb08e262df1
DarkAce65 <tvdood99@gmail.com>
2021-12-25 15:56:22 -0500

    Move parse logic to main

--- 2021/day24.py
+++ 2021/day24.py
@@ -2,7 +2,22 @@ from collections import deque
 from pathlib import Path
 
 
-def part_one() -> int:
+def part_one(digit_candidates: list[set[int]]) -> int:
+    model_number = ""
+    for candidates in digit_candidates:
+        model_number += str(max(candidates))
+
+    return int(model_number)
+
+
+def part_two(digit_candidates: list[set[int]]) -> int:
+    pass
+
+
+if __name__ == "__main__":
+    with open(Path(__file__).stem + ".input.txt", "r", encoding="utf-8") as file:
+        problem_input = [line.rstrip() for line in file]
+
     a_values = [1, 1, 1, 1, 26, 1, 26, 26, 1, 26, 1, 26, 26, 26]
     b_values = [10, 15, 14, 15, -8, 10, -16, -4, 11, -3, 12, -7, -15, -7]
     c_values = [2, 16, 9, 0, 1, 12, 6, 6, 3, 5, 9, 3, 2, 3]
@@ -36,20 +51,5 @@ def part_one() -> int:
                 }
             )
 
-    model_number = ""
-    for candidates in digit_candidates:
-        model_number += str(max(candidates))
-
-    return int(model_number)
-
-
-def part_two(problem_input: list[str]) -> int:
-    pass
-
-
-if __name__ == "__main__":
-    with open(Path(__file__).stem + ".input.txt", "r", encoding="utf-8") as file:
-        problem_input = [line.rstrip() for line in file]
-
-    print("Part One: ", part_one())
-    print("Part Two: ", part_two(problem_input))
+    print("Part One: ", part_one(digit_candidates))
+    print("Part Two: ", part_two(digit_candidates))
