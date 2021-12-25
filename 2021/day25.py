c35dfb2b914807b0a26da666a9d22bc725e5a9c1
DarkAce65 <tvdood99@gmail.com>
2021-12-25 01:06:46 -0500

    Solve day 25 part 1

---
+++ 2021/day25.py
@@ -0,0 +1,105 @@
+from __future__ import annotations
+
+import copy
+from enum import Enum, unique
+from pathlib import Path
+
+
+@unique
+class Space(str, Enum):
+    EMPTY = "EMPTY"
+    EAST = "EAST"
+    SOUTH = "SOUTH"
+
+    def from_input(space: str) -> Space:
+        if space == ".":
+            return Space.EMPTY
+        elif space == ">":
+            return Space.EAST
+        elif space == "v":
+            return Space.SOUTH
+
+        raise ValueError(f"Unknown space value: {space}")
+
+    def __repr__(self) -> str:
+        if self == Space.EMPTY:
+            return "."
+        if self == Space.EAST:
+            return ">"
+        if self == Space.SOUTH:
+            return "v"
+
+        raise ValueError(f"Unknown space value: {self}")
+
+
+def parse_sea_floor(problem_input: list[str]) -> list[list[Space]]:
+    sea_floor = []
+    for line in problem_input:
+        row = []
+        for space in line:
+            row.append(Space.from_input(space))
+        sea_floor.append(row)
+
+    return sea_floor
+
+
+def part_one(sea_floor: list[list[Space]]) -> int:
+    step = 0
+    width = len(sea_floor[0])
+    height = len(sea_floor)
+    while True:
+        step += 1
+        locations_to_move: set[tuple[int, int]] = set()
+        for row in range(height):
+            for col in range(width):
+                space = sea_floor[row][col]
+                if space == Space.EMPTY:
+                    continue
+
+                if space == Space.EAST:
+                    if sea_floor[row][(col + 1) % width] == Space.EMPTY:
+                        locations_to_move.add((row, col))
+                elif space == Space.SOUTH:
+                    if (
+                        sea_floor[(row + 1) % height][col] == Space.EMPTY
+                        and sea_floor[(row + 1) % height][(col - 1 + width) % width]
+                        != Space.EAST
+                    ) or (
+                        sea_floor[(row + 1) % height][col] == Space.EAST
+                        and sea_floor[(row + 1) % height][(col + 1) % width]
+                        == Space.EMPTY
+                    ):
+                        locations_to_move.add((row, col))
+
+        if len(locations_to_move) == 0:
+            break
+
+        next_sea_floor = copy.deepcopy(sea_floor)
+        for (row, col) in locations_to_move:
+            space = sea_floor[row][col]
+            if space == Space.EAST:
+                if space == next_sea_floor[row][col]:
+                    next_sea_floor[row][col] = Space.EMPTY
+                next_sea_floor[row][(col + 1) % width] = Space.EAST
+            elif space == Space.SOUTH:
+                if space == next_sea_floor[row][col]:
+                    next_sea_floor[row][col] = Space.EMPTY
+                next_sea_floor[(row + 1) % height][col] = Space.SOUTH
+
+        sea_floor = next_sea_floor
+
+    return step
+
+
+def part_two(problem_input: list[str]) -> int:
+    pass
+
+
+if __name__ == "__main__":
+    with open(Path(__file__).stem + ".input.txt", "r", encoding="utf-8") as file:
+        problem_input = [line.rstrip() for line in file]
+
+    sea_floor = parse_sea_floor(problem_input)
+
+    print("Part One: ", part_one(sea_floor))
+    print("Part Two: ", part_two(problem_input))
