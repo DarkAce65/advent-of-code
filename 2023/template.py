29848d00ae7d9a17d49977ff58eb845e8ed63a45
DarkAce65 <tvdood99@gmail.com>
2023-11-30 18:13:31 -0500

    Set up for 2023

---
+++ 2023/template.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+
+def part_one(problem_input: list[str]) -> int:
+    return 0
+
+
+def part_two(problem_input: list[str]) -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    with open(Path(__file__).with_suffix(".input.txt"), "r", encoding="utf-8") as file:
+        problem_input = [line.rstrip() for line in file]
+
+    print("Part One: ", part_one(problem_input))
+    print("Part Two: ", part_two(problem_input))
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Solutions for https://adventofcode.com/
 
 - [2021](./2021) - Python
 - [2022](./2022) - Python
+- [2023](./2023) - Python
