3b391437f3a2ea975c46a0d7aef6374c6b93f1b1
DarkAce65 <tvdood99@gmail.com>
2023-12-02 00:38:24 -0500

    Solve day 2

---
+++ 2023/day2.py
@@ -0,0 +1,62 @@
+import re
+from pathlib import Path
+
+
+def make_cube_pair(cube_pair: str) -> tuple[int, str]:
+    num, color = cube_pair.strip().split(" ")
+    return (int(num), color)
+
+
+class Game:
+    id: int
+    rounds: list[dict[str, int]]
+
+    def __init__(self, game_str: str) -> None:
+        match = re.match(r"Game (\d+): (.*)", game_str)
+        assert match is not None
+        self.id = int(match.group(1))
+        self.rounds = []
+        for round_str in match.group(2).split(";"):
+            round = {}
+            for cube_pair in round_str.strip().split(","):
+                num, color = cube_pair.strip().split(" ")
+                round[color] = int(num)
+            self.rounds.append(round)
+
+    def compute_power(self) -> int:
+        red = max((round["red"] if "red" in round else 0) for round in self.rounds)
+        green = max((round["green"] if "green" in round else 0) for round in self.rounds)
+        blue = max((round["blue"] if "blue" in round else 0) for round in self.rounds)
+
+        return red * green * blue
+
+
+# 12 red cubes, 13 green cubes, 14 blue cubes
+def part_one(games: list[Game]) -> int:
+    sum = 0
+    for game in games:
+        if all(
+            ("red" not in round or round["red"] <= 12)
+            and ("green" not in round or round["green"] <= 13)
+            and ("blue" not in round or round["blue"] <= 14)
+            for round in game.rounds
+        ):
+            sum += game.id
+
+    return sum
+
+
+def part_two(games: list[Game]) -> int:
+    return sum(game.compute_power() for game in games)
+
+
+if __name__ == "__main__":
+    with open(Path(__file__).with_suffix(".input.txt"), "r", encoding="utf-8") as file:
+        problem_input = [line.rstrip() for line in file]
+
+    games = []
+    for game in problem_input:
+        games.append(Game(game))
+
+    print("Part One: ", part_one(games))
+    print("Part Two: ", part_two(games))
