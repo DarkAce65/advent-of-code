867831d7752330a97d78f5f2b0e0f97714114329
DarkAce65 <tvdood99@gmail.com>
2023-12-03 00:58:16 -0500

    Clean up and simplify day 3

--- 2023/day3.py
+++ 2023/day3.py
@@ -5,80 +5,65 @@ from pathlib import Path
 @dataclass
 class Part:
     part_number: str
-    is_valid: bool
     last_position: tuple[int, int]
 
     def __hash__(self) -> int:
         return self.last_position.__hash__()
 
 
-def part_one(problem_input: list[str]) -> int:
+def build_parts_list(schematic: list[str]) -> tuple[list[Part], dict[str, Part]]:
     parts: list[Part] = []
-    locations: dict[str, Part] = {}
-    symbol_locations: list[tuple[int, int]] = []
-    for row in range(len(problem_input)):
-        for col in range(len(problem_input[row])):
-            if problem_input[row][col].isnumeric():
+    part_locations: dict[str, Part] = {}
+    for row in range(len(schematic)):
+        for col in range(len(schematic[row])):
+            if schematic[row][col].isnumeric():
                 connected_part = parts[-1] if len(parts) > 0 else None
                 if connected_part is None or connected_part.last_position != (
                     row,
                     col - 1,
                 ):
-                    connected_part = Part(problem_input[row][col], False, (row, col))
+                    connected_part = Part(schematic[row][col], (row, col))
                     parts.append(connected_part)
                 else:
-                    connected_part.part_number += problem_input[row][col]
+                    connected_part.part_number += schematic[row][col]
                     connected_part.last_position = (row, col)
-                locations[str(row) + "-" + str(col)] = connected_part
-            elif (
-                not problem_input[row][col].isnumeric() and problem_input[row][col] != "."
-            ):
-                symbol_locations.append((row, col))
+                part_locations[str(row) + "-" + str(col)] = connected_part
 
-    for symbol_location in symbol_locations:
-        s_row, s_col = symbol_location
-        for row in range(s_row - 1, s_row + 2):
-            for col in range(s_col - 1, s_col + 2):
-                location_key = str(row) + "-" + str(col)
-                if location_key in locations:
-                    locations[location_key].is_valid = True
+    return (parts, part_locations)
 
-    return sum(int(part.part_number) for part in parts if part.is_valid)
+
+def part_one(problem_input: list[str]) -> int:
+    _, part_locations = build_parts_list(problem_input)
+
+    valid_parts: set[Part] = set()
+    for row in range(len(problem_input)):
+        for col in range(len(problem_input[row])):
+            if not problem_input[row][col].isnumeric() and problem_input[row][col] != ".":
+                for r in range(row - 1, row + 2):
+                    for c in range(col - 1, col + 2):
+                        location_key = str(r) + "-" + str(c)
+                        if location_key in part_locations:
+                            valid_parts.add(part_locations[location_key])
+
+    return sum(int(part.part_number) for part in valid_parts)
 
 
 def part_two(problem_input: list[str]) -> int:
-    parts: list[Part] = []
-    locations: dict[str, Part] = {}
-    potential_gear_locations: list[tuple[int, int]] = []
-    for row in range(len(problem_input)):
-        for col in range(len(problem_input[row])):
-            if problem_input[row][col].isnumeric():
-                connected_part = parts[-1] if len(parts) > 0 else None
-                if connected_part is None or connected_part.last_position != (
-                    row,
-                    col - 1,
-                ):
-                    connected_part = Part(problem_input[row][col], False, (row, col))
-                    parts.append(connected_part)
-                else:
-                    connected_part.part_number += problem_input[row][col]
-                    connected_part.last_position = (row, col)
-                locations[str(row) + "-" + str(col)] = connected_part
-            elif problem_input[row][col] == "*":
-                potential_gear_locations.append((row, col))
+    _, part_locations = build_parts_list(problem_input)
 
     sum = 0
-    for gear_location in potential_gear_locations:
-        connected_parts: set[Part] = set()
-        g_row, g_col = gear_location
-        for row in range(g_row - 1, g_row + 2):
-            for col in range(g_col - 1, g_col + 2):
-                location_key = str(row) + "-" + str(col)
-                if location_key in locations:
-                    connected_parts.add(locations[location_key])
-        if len(connected_parts) == 2:
-            first, second = [int(p.part_number) for p in connected_parts]
-            sum += first * second
+    for row in range(len(problem_input)):
+        for col in range(len(problem_input[row])):
+            if problem_input[row][col] == "*":
+                connected_parts: set[Part] = set()
+                for r in range(row - 1, row + 2):
+                    for c in range(col - 1, col + 2):
+                        location_key = str(r) + "-" + str(c)
+                        if location_key in part_locations:
+                            connected_parts.add(part_locations[location_key])
+                if len(connected_parts) == 2:
+                    first, second = [int(p.part_number) for p in connected_parts]
+                    sum += first * second
 
     return sum
 
