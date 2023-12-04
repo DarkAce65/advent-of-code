4d0fc229a8995e51b43f13efcaf71c1e12a718ee
DarkAce65 <tvdood99@gmail.com>
2023-12-04 00:32:11 -0500

    Clean up and simplify day 4

--- 2023/day4.py
+++ 2023/day4.py
@@ -3,17 +3,26 @@ from collections import defaultdict
 from pathlib import Path
 
 
+def parse_card(card_line: str) -> tuple[int, set[str], list[str]]:
+    card_str, all_numbers_str = card_line.split(":", maxsplit=2)
+    card_number_match = re.match(r"Card\s+(\d+)", card_str)
+    assert card_number_match is not None
+    card_number = int(card_number_match.group(1))
+    winning_numbers_str, numbers_str = all_numbers_str.split("|")
+
+    return (
+        card_number,
+        set(winning_numbers_str.strip().split()),
+        numbers_str.strip().split(),
+    )
+
+
 def part_one(problem_input: list[str]) -> int:
     points = 0
     for line in problem_input:
-        _, numbers_str = line.split(":", maxsplit=2)
-        winning_numbers_str, numbers = numbers_str.split("|")
+        _, winning_numbers, numbers = parse_card(line)
 
-        winning_numbers = set(winning_numbers_str.strip().split())
-        wins = 0
-        for number in numbers.strip().split():
-            if number in winning_numbers:
-                wins += 1
+        wins = sum(1 for number in numbers if number in winning_numbers)
         if wins > 0:
             points += 2 ** (wins - 1)
 
@@ -23,17 +32,10 @@ def part_one(problem_input: list[str]) -> int:
 def part_two(problem_input: list[str]) -> int:
     card_counts: dict[int, int] = defaultdict(int)
     for line in problem_input:
-        card_str, numbers_str = line.split(":", maxsplit=2)
-        card_number = int(re.match(r"Card\s+(\d+)", card_str).group(1))
-        winning_numbers_str, numbers = numbers_str.split("|")
-
-        winning_numbers = set(winning_numbers_str.strip().split())
-        wins = 0
-        for number in numbers.strip().split():
-            if number in winning_numbers:
-                wins += 1
+        card_number, winning_numbers, numbers = parse_card(line)
 
         card_counts[card_number] += 1
+        wins = sum(1 for number in numbers if number in winning_numbers)
         for w in range(wins):
             card_counts[card_number + w + 1] += card_counts[card_number]
 
