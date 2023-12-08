628962ce3ca5677903121391da9b72263b771936
DarkAce65 <tvdood99@gmail.com>
2023-12-08 00:52:20 -0500

    Solve day 8

---
+++ 2023/day8.py
@@ -0,0 +1,60 @@
+import math
+import re
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass
+class Node:
+    name: str
+    left: str
+    right: str
+
+
+def part_one(instructions: str, nodes: dict[str, Node]) -> int:
+    current = "AAA"
+    steps = 0
+    while current != "ZZZ":
+        direction = instructions[steps % len(instructions)]
+        if direction == "L":
+            current = nodes[current].left
+        elif direction == "R":
+            current = nodes[current].right
+        steps += 1
+
+    return steps
+
+
+def part_two(instructions: str, nodes: dict[str, Node]) -> int:
+    current_nodes = [name for name in nodes.keys() if name.endswith("A")]
+
+    distances: dict[str, int] = {}
+    for node in current_nodes:
+        current = node
+        steps = 0
+        while not current.endswith("Z"):
+            direction = instructions[steps % len(instructions)]
+            if direction == "L":
+                current = nodes[current].left
+            elif direction == "R":
+                current = nodes[current].right
+            steps += 1
+        distances[node] = steps
+
+    return math.lcm(*distances.values())
+
+
+if __name__ == "__main__":
+    with open(Path(__file__).with_suffix(".input.txt"), "r", encoding="utf-8") as file:
+        problem_input = [line.rstrip() for line in file]
+
+    instructions = problem_input[0]
+    nodes: dict[str, Node] = {}
+    for line in problem_input[2:]:
+        match = re.match(r"(\w+) = \((\w+), (\w+)\)", line)
+        if match is not None:
+            name = match.group(1)
+            nodes[name] = Node(name, match.group(2), match.group(3))
+
+    print("Part One: ", part_one(instructions, nodes))
+    print("Part Two: ", part_two(instructions, nodes))
