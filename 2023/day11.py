d328f0c7498f4d824693a95be818bbf854363330
DarkAce65 <tvdood99@gmail.com>
2023-12-11 00:36:26 -0500

    Clean up solution and convert part one to generic solution

--- 2023/day11.py
+++ 2023/day11.py
@@ -1,73 +1,40 @@
 from utils import get_and_cache_input
 
 
-def part_one(problem_input: list[str]) -> int:
-    expanded_image: list[str] = []
-
-    columns_to_expand = set(range(len(problem_input[0])))
-    for row, line in enumerate(problem_input):
-        row_empty = True
-        for col, pixel in enumerate(problem_input[row]):
-            if pixel == "#":
-                row_empty = False
-                if col in columns_to_expand:
-                    columns_to_expand.remove(col)
-
-        expanded_image.append(line)
-        if row_empty:
-            expanded_image.append("." * len(line))
-    for col in sorted(columns_to_expand, reverse=True):
-        for row in range(len(expanded_image)):
-            expanded_image[row] = (
-                expanded_image[row][:col] + "." + expanded_image[row][col:]
-            )
-
+def compute_galaxy_distances(problem_input: list[str], expansion_multiplier: int) -> int:
     galaxies: list[tuple[int, int]] = []
-    for row, line in enumerate(expanded_image):
-        for col, pixel in enumerate(expanded_image[row]):
-            if pixel == "#":
-                galaxies.append((row, col))
-
-    result = 0
-    for index, (row, col) in enumerate(galaxies):
-        result += sum(abs(row - r) + abs(col - c) for r, c in galaxies[index:])
-
-    return result
-
-
-def part_two(problem_input: list[str]) -> int:
-    expansion_count = 1_000_000
-
-    rows_to_expand = set()
-    columns_to_expand = set(range(len(problem_input[0])))
-    for row, _ in enumerate(problem_input):
-        row_empty = True
-        for col, pixel in enumerate(problem_input[row]):
-            if pixel == "#":
-                row_empty = False
-                if col in columns_to_expand:
-                    columns_to_expand.remove(col)
-        if row_empty:
-            rows_to_expand.add(row)
-
-    galaxies: list[tuple[int, int]] = []
+    empty_rows = set(range(len(problem_input)))
+    empty_columns = set(range(len(problem_input[0])))
     for row, _ in enumerate(problem_input):
         for col, pixel in enumerate(problem_input[row]):
             if pixel == "#":
                 galaxies.append((row, col))
+
+                if row in empty_rows:
+                    empty_rows.remove(row)
+                if col in empty_columns:
+                    empty_columns.remove(col)
 
     result = 0
     for index, (row, col) in enumerate(galaxies):
         for other_row, other_col in galaxies[index:]:
-            distance = 0
-            for r in range(min(row, other_row), max(row, other_row)):
-                distance += expansion_count if r in rows_to_expand else 1
-            for c in range(min(col, other_col), max(col, other_col)):
-                distance += expansion_count if c in columns_to_expand else 1
-
-            result += distance
+            result += sum(
+                (expansion_multiplier if r in empty_rows else 1)
+                for r in range(min(row, other_row), max(row, other_row))
+            ) + sum(
+                (expansion_multiplier if c in empty_columns else 1)
+                for c in range(min(col, other_col), max(col, other_col))
+            )
 
     return result
+
+
+def part_one(problem_input: list[str]) -> int:
+    return compute_galaxy_distances(problem_input, 2)
+
+
+def part_two(problem_input: list[str]) -> int:
+    return compute_galaxy_distances(problem_input, 1_000_000)
 
 
 if __name__ == "__main__":
