fef9e5cabe76fc8a776825742e419df88cab9710
DarkAce65 <tvdood99@gmail.com>
2024-12-01 00:36:10 -0500

    Initialize 2024

---
+++ 2024/template.py
@@ -0,0 +1,16 @@
+from utils import get_and_cache_input
+
+
+def part_one(problem_input: list[str]) -> int:
+    return 0
+
+
+def part_two(problem_input: list[str]) -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    problem_input = get_and_cache_input(__file__)
+
+    print("Part One: ", part_one(problem_input))
+    print("Part Two: ", part_two(problem_input))
---
+++ 2024/utils.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+import requests
+from dotenv import dotenv_values
+
+
+def get_and_cache_input(file_path_str: str) -> list[str]:
+    file_path = Path(file_path_str)
+    input_file_path = file_path.with_suffix(".input.txt")
+    if input_file_path.exists() and input_file_path.stat().st_size > 0:
+        with open(input_file_path, "r", encoding="utf-8") as file:
+            return [line.rstrip() for line in file]
+
+    year = file_path.parent.stem
+    day = file_path.stem.removeprefix("day")
+    assert (
+        year.isnumeric() and day.isnumeric()
+    ), f'Failed to parse year "{year}" and day "{day}"'
+
+    session_token = dotenv_values(file_path.parent.with_name(".env")).get("AOC_SESSION")
+    if session_token is None:
+        raise ValueError("Missing session token")
+
+    res = requests.get(
+        f"https://adventofcode.com/{year}/day/{day}/input",
+        cookies={"session": session_token},
+    )
+    if res.status_code != 200:
+        raise ValueError(
+            f"Failed to retrieve input for year {year}, day {day} (status code: {res.status_code})"
+        )
+
+    with open(input_file_path, "w+", encoding="utf-8") as file:
+        file.write(res.text)
+        file.seek(0)
+        return [line.rstrip() for line in file]
